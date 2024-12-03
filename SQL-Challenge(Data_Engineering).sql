63b8db6762c4d899638d43399ac7470f9be0dc4b
C1-10P <casey_wright@outlook.com>
2024-12-02 17:58:27 -0800

    Made a few small changes

--- SQL-Challenge(Data_Engineering).sql
+++ SQL-Challenge(Data_Engineering).sql
@@ -4,35 +4,35 @@
 -- columns.
 
 Create Table departments (
-    dept_no VARCHAR Primary Key,
-    dept_name VARCHAR Not Null
+    dept_no VARCHAR(4) Primary Key,
+    dept_name VARCHAR(50) Not Null
 );
 
 Create Table employees (
     emp_no INTEGER Primary Key,
-    emp_title_id VARCHAR,
+    emp_title_id VARCHAR(10),
     birth_date DATE,
-    first_name VARCHAR,
-    last_name VARCHAR,
-    sex VARCHAR,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    sex VARCHAR(1),
     hire_date DATE
 );
 
 Create Table titles (
-    title_id VARCHAR Primary Key,
-    title VARCHAR Not Null
+    title_id VARCHAR(10) Primary Key,
+    title VARCHAR(50) Not Null
 );
 
 Create Table dept_emp (
     emp_no INTEGER,
-    dept_no VARCHAR,
+    dept_no VARCHAR(4),
     Primary Key (emp_no, dept_no),
     Foreign Key (emp_no) References employees(emp_no),
     Foreign Key (dept_no) References departments(dept_no)
 );
 
 Create Table dept_manager (
-    dept_no VARCHAR,
+    dept_no VARCHAR(4),
     emp_no INTEGER,
     Primary Key (dept_no, emp_no),
     Foreign Key (dept_no) References departments(dept_no),
